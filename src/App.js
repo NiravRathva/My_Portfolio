e199112ffb216008a0fd5341668294dc14ed6769
NiravRathva <niravrathwa57@gmail.com>
2023-09-23 10:13:02 +0530

    added tailwind css configuration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello World</h1>
+    <div className="App bg-red-900">
+     <h1 className="text-blue-800">Hello World</h1>
     </div>
   );
 }
