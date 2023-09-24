7741ed569d9fc39bca69758074e825b603118cfd
NiravRathva <niravrathwa57@gmail.com>
2023-09-24 09:57:11 +0530

    refactored Project component's links

--- src/Component/Projects.js
+++ src/Component/Projects.js
@@ -2,15 +2,16 @@ import { projectsData } from "../ProjectData";
 
 const Projects = () => {
   return (
-    <div id="Projects" className="container mx-auto min-h-screen">
+    <div id="Projects" className="container mx-auto h-full mb-2 ">
       <div className="text-center">
         <h1 className="text-4xl font-bold mt-8">Some Of My Recent Projects</h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4 ">
         {projectsData.map((project) => (
           <div
             key={project.id}
-            className="bg-gray-100 rounded-lg shadow-lg p-4 h-fit hover:shadow-xl transition duration-300"
+            className="bg-gray-100 rounded-lg shadow-lg p-4 hover:shadow-xl transition duration-300 relative z-[-1] h-fit"
+            
           >
             <img
               src={project.img}
@@ -19,12 +20,28 @@ const Projects = () => {
             />
             <h2 className="text-xl font-semibold mt-2">{project.name}</h2>
             <p className="text-gray-600">{project.description}</p>
-            <button className="bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded-md m-2">
-              View Code
-            </button>
-            <button className="bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded-md m-2">
-              Visit Website
-            </button>
+            <div className="my-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2">
+              <a
+                href={project.frontendCode}
+                className="bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded-md "
+              >
+                Frontend code
+              </a>
+              <a
+                href={project.frontendCode}
+                className="bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded-md "
+              >
+                Backend code
+              </a>
+              <a
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded-md "
+              >
+                Visit Website
+              </a>
+            </div>
           </div>
         ))}
       </div>
--- src/ProjectData.js
+++ src/ProjectData.js
@@ -1,36 +1,38 @@
-
-export const projectsData=[
-
-    {
-        id:"1",
-        img :"Booking",
-        name:"Booking Adviser",
-        description:"Developed a booking website where user can search for a hotel and select dates and room quantity.",
-        link:"/"
-
-    },
-    {
-        id:"2",
-        img :"Youtube",
-        name:"Youtube Clone",
-        description:"Developed  youtube clone website, where user can signin , add a video , search video etc",
-        link:"/"
-
-    },
-    {
-        id:"3",
-        img :"Blog",
-        name:"Blog App",
-        description:"developed a blog website with login , register page , write blog page .",
-        link:"/"
-
-    },
-    {
-        id:"4",
-        img :"Crud",
-        name:"Crud App",
-        description:"developed a basic crud website where user can add data update data and delete data",
-        link:"/"
-
-    },
-]
+import localServices from "./Images/localServices.png";
+import bg from "./Images/bg1.jpg";
+export const projectsData = [
+  {
+    id: "1",
+    img: localServices,
+    name: "Local Services",
+    description:
+      "Developed a booking website where user can search for a hotel and select dates and room quantity.",
+    link: "https://local-services-frontend-niravrathva.vercel.app/",
+    frontendCode: "https://github.com/NiravRathva/local-services-frontend.git",
+    backendCode: "https://github.com/NiravRathva/localServices.git",
+  },
+  {
+    id: "2",
+    img: bg,
+    name: "Youtube Clone",
+    description:
+      "Developed  youtube clone website, where user can signin , add a video , search video etc",
+    link: "/",
+  },
+  {
+    id: "3",
+    img: "Blog",
+    name: "Blog App",
+    description:
+      "developed a blog website with login , register page , write blog page .",
+    link: "/",
+  },
+  {
+    id: "4",
+    img: bg,
+    name: "Crud App",
+    description:
+      "developed a basic crud website where user can add data update data and delete data",
+    link: "/",
+  },
+];
