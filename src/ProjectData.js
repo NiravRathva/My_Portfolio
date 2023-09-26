0dc68494b9461cae564f4df65027483ae0f58cca
NiravRathva <niravrathwa57@gmail.com>
2023-09-26 13:48:06 +0530

    refactored Projectdata

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -1,38 +1,33 @@
 import localServices from "./Images/localServices.png";
-import bg from "./Images/bg1.jpg";
+import insta from "./Images/insta.png"
+import flipkart from "./Images/flipkart.png"
 export const projectsData = [
   {
     id: "1",
     img: localServices,
     name: "Local Services",
     description:
-      "Developed a booking website where user can search for a hotel and select dates and room quantity.",
+      "Developed 'Local Services,' a MERN stack application allowing users to register, log in, and choose from various in-home services such as plumbing, cleaning, and electrical work, each with customizable packages. The app provides a user-friendly experience with Bootstrap for responsive design, while Redux manages state efficiently. Axios facilitates seamless data exchange, and Redux Persist ensures persistent data storage. For secure payments, the app integrates Stripe, enabling users to make payments conveniently. ",
     link: "https://local-services-frontend-niravrathva.vercel.app/",
     frontendCode: "https://github.com/NiravRathva/local-services-frontend.git",
     backendCode: "https://github.com/NiravRathva/localServices.git",
   },
   {
     id: "2",
-    img: bg,
+    img: insta,
     name: "Instagram Clone",
     description:
-      "Developed  youtube clone website, where user can signin , add a video , search video etc",
-    link: "/",
+      "Developed an Instagram clone using the MERN stack, offering essential features like user registration, login, post creation, and the ability to follow other users. The application showcases a sleek and responsive user interface, meticulously styled with Tailwind CSS for a modern aesthetic. It incorporates robust state management through Redux, ensuring a seamless user experience. Additionally, Axios handles HTTP requests, and Redux Persist enables data persistence, enhancing data retrieval and user engagement.",
+      frontendCode: "https://github.com/NiravRathva/Instagram-clone-frontend.git",
+      backendCode:"https://github.com/NiravRathva/Instagram-Clone-Backend.git"
   },
   {
     id: "3",
-    img: "Blog",
-    name: "Blog App",
+    img: flipkart,
+    name: "Flipkart Clone",
     description:
-      "developed a blog website with login , register page , write blog page .",
-    link: "/",
-  },
-  {
-    id: "4",
-    img: bg,
-    name: "Crud App",
-    description:
-      "developed a basic crud website where user can add data update data and delete data",
-    link: "/",
+      " Building a Flipkart clone using the MERN stack.Backend API is nearly complete, pending some adjustments for seamless integration with the frontend. Currently, actively engaged in frontend development to deliver a comprehensive e-commerce solution.",
+    frontendCode: "https://github.com/NiravRathva/Flipkart-clone.git",
+    backendCode:"https://github.com/NiravRathva/flipkart-clone-backend.git"
   },
 ];
