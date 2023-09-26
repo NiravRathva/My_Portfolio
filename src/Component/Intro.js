52ef8c63be561dc895e68236736ef3d76c917935
NiravRathva <niravrathwa57@gmail.com>
2023-09-26 14:48:51 +0530

    implemented fort-weight for small screen device also

--- src/Component/Intro.js
+++ src/Component/Intro.js
@@ -6,7 +6,7 @@ const Intro = () => {
       <h1 className="text-lg md:text-xl lg:text-3xl xl:text-5xl font-bold lg:font-black text-gray-800 my-2">
         I'm Nirav Rathwa
       </h1>
-      <h1 className="text-base md:text-lg lg:text-2xl xl:text-4xl text-gray-800 lg:font-black my-2">
+      <h1 className="text-base md:text-lg lg:text-2xl xl:text-4xl text-gray-800 font-black my-2">
         And I'm a Web Developer
       </h1>
       <button className="bg-gray-300 hover:bg-gray-400 py-2 px-4 rounded-lg text-gray-800 font-bold text-sm md:text-base mt-4 transition duration-300 my-2">
